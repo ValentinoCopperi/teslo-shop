a236269d8b4712b7b3a250f9d9f7cf098ce9a4b8
copep <valencope100@gmail.com>
2024-07-09 00:14:01 -0300

    components fixed 3

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -3,27 +3,27 @@
 import { titleFont } from "@/config/fonts"
 import { useAuthContext } from "@/context/AuthContext"
 import Link from "next/link"
-import { FormEvent } from "react"
 import { useForm } from "react-hook-form"
+
 export default function Login() {
 
-    const {authLogin,warning} = useAuthContext()
+    const { authLogin, warning } = useAuthContext()
 
     const {
-        register : registerLogin,
-        handleSubmit : loginSubmit,
-        formState : {errors : errorsLogin}
+        register: registerLogin,
+        handleSubmit: loginSubmit,
+        formState: { errors: errorsLogin }
     } = useForm({
-        defaultValues : {
-            email : '',
-            password : ''
+        defaultValues: {
+            email: '',
+            password: ''
         }
     })
 
     const handleSubmit = loginSubmit((data) => {
         const datos = {
-            email : data.email,
-            password : data.password
+            email: data.email,
+            password: data.password
         }
         authLogin(datos)
     }) 
@@ -32,23 +32,22 @@ export default function Login() {
         <main className="w-full h-screen flex flex-col items-center justify-center bg-gray-50 sm:px-4">
             <div className="w-full space-y-6 text-gray-600 sm:max-w-md">
                 <div className="text-center">
-                <div>
+                    <div>
                         <Link
                             href="/"
                             className="text-gray-800 text-2xl  sm:text-3xl"
                         >
-                            <span className={`${titleFont.className}antialised font-bold`}>Teslo</span>
+                            <span className={`${titleFont.className} antialised font-bold`}>Teslo</span>
 
                             <span> | Shop</span>
                         </Link>
                     </div>
                     <div className="mt-5 space-y-2">
                         <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">Log in to your account</h3>
-                        <p className="">Don't have an account? <Link href="/auth/new-account" className="font-medium text-indigo-600 hover:text-indigo-500">Sign up</Link></p>
+                        <p className="">Don&apos;t have an account? <Link href="/auth/new-account" className="font-medium text-indigo-600 hover:text-indigo-500">Sign up</Link></p>
                     </div>
                 </div>
                 <div className="bg-white shadow p-4 py-6 space-y-8 sm:p-6 sm:rounded-lg">
-                   
                     <form
                         onSubmit={handleSubmit}
                         className="space-y-5"
@@ -71,12 +70,11 @@ export default function Login() {
                                             value: /^[a-zA-Z0-9_.+-]+@+[a-zA-Z0-9-]+\.[a-zA-Z0-9]/,
                                             message: 'Must be email format valid'
                                         }
-
                                     })
-                                    } 
+                                }
                             />
                             {
-                                    errorsLogin.email && <span className='text-red-400'>{errorsLogin.email?.message}</span>
+                                errorsLogin.email && <span className='text-red-400'>{errorsLogin.email?.message}</span>
                             }
                         </div>
                         <div>
@@ -94,11 +92,11 @@ export default function Login() {
                                             message: 'Password is required'
                                         }
                                     })
-                                    }
+                                }
                             />
                             {
-                                    errorsLogin.password && <span className='text-red-400'>{errorsLogin.password?.message}</span>
-                             }
+                                errorsLogin.password && <span className='text-red-400'>{errorsLogin.password?.message}</span>
+                            }
                         </div>
                         <button
                             className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
