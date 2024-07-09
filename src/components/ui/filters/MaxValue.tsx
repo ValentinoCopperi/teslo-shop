60f53136682e5b964531d2ce5e58ebb2cb4d94a9
copep <valencope100@gmail.com>
2024-07-09 00:04:44 -0300

    filters fixed

--- src/components/ui/filters/MaxValue.tsx
+++ src/components/ui/filters/MaxValue.tsx
@@ -2,24 +2,27 @@
 import React from 'react'
 import { useStoreFilter } from '@/store'
 import Slider from '@mui/material/Slider';
+
 export const MaxValue = () => {
-    const maxValue = useStoreFilter(state => state.maxValue)
-    const handleMaxValue = useStoreFilter(state => state.handleMaxValue)
-    const handleMaxValueInput =  ( e : EventTarget) =>{
-      handleMaxValue(e.target.value)
-    }
+  const maxValue = useStoreFilter(state => state.maxValue)
+  const handleMaxValue = useStoreFilter(state => state.handleMaxValue)
+
+  const handleMaxValueInput = (event: Event, value: number | number[]) => {
+    handleMaxValue(value as number)
+  }
+
   return (
     <div className='flex items-center'>
-        <Slider
-          aria-label="Temperature"
-          min={10}
-          max={300}
-          step={10}
-          onChange={handleMaxValueInput}
-          color="primary"
-          value={maxValue}
-        />
-        <span className='pl-3'>{maxValue}</span>
+      <Slider
+        aria-label="Temperature"
+        min={10}
+        max={300}
+        step={10}
+        onChange={handleMaxValueInput}
+        color="primary"
+        value={maxValue}
+      />
+      <span className='pl-3'>{maxValue}</span>
     </div>
   )
 }
